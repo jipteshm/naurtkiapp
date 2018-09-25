c1d4f956717ab912ebac4034910fa89915979c9a
jipteshm <jiptesh@gmail.com>
2018-09-25 22:36:11 +0530

    first commit

---
+++ src/main/java/com/naurtki/naurtkiapp/NaurtkiappApplication.java
@@ -0,0 +1,12 @@
+package com.naurtki.naurtkiapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaurtkiappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaurtkiappApplication.class, args);
+	}
+}
